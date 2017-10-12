c3c9e220d1fa891296252e577e9b4eafb20504c0
Eric McKinney <mckinney99@gmail.com>
2017-10-12 17:17:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,18 @@
 def turn_count(board)
   counter = 0
-  if turn  =+ 1
-    counter +=1
+  board.each do |space|
+    if space == "X" || space == "0"
+      counter += 1
 end
+end
+
+
+def current_player(board)
+  total_turns = turn_count(board)
+  if total_turns % 2 == 0
+    return "X"
+  else
+    return "0"
+  end
+end
+
